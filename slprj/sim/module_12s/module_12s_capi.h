9f261c12cb2d37936b6038853e7cd2a9b94adc92
dhia <dhia.hamza2014@gmail.com>
2026-04-02 02:37:58 +0200

    Final cleanup of private and temporary files

--- slprj/sim/module_12s/module_12s_capi.h
+++
@@ -1,6 +0,0 @@
-#ifndef module_12s_capi_h_
-#define module_12s_capi_h_
-#include "module_12s.h"
-extern void module_12s_InitializeDataMapInfo ( o3m5zhn2ea * const mhbwal3yff
-, jj5jikjqb0 * localDW , void * sysRanPtr , int contextTid ) ;
-#endif
